832a72bfcf4d2f04ffc0ab72e953973fb32ad1e4
BlacuttMatias <matiasblacutt16@gmail.com>
2022-07-31 02:12:08 -0300

    Add video player component

---
+++ app/src/main/java/com/example/instructorlsa/ui/screens/learningSection/signLearning/components/VideoPlayer.kt
@@ -0,0 +1,56 @@
+package com.example.instructorlsa.ui.screens.learningSection.signLearning.components
+
+import android.net.Uri
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.ui.PlayerView
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.google.android.exoplayer2.util.Util
+
+@Composable
+fun VideoPlayer(urlVideo: String){
+    
+    Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
+
+        // Fetching the Local Context
+        val mContext = LocalContext.current
+
+        // Declaring ExoPlayer
+        val mExoPlayer = remember(mContext) {
+            ExoPlayer.Builder(mContext).build().apply {
+                val dataSourceFactory = DefaultDataSourceFactory(mContext, Util.getUserAgent(mContext, mContext.packageName))
+                val source = ProgressiveMediaSource.Factory(dataSourceFactory).createMediaSource(
+                    MediaItem.fromUri(urlVideo))
+                prepare(source)
+            }
+        }
+
+        // Implementing ExoPlayer
+        AndroidView(factory = { context ->
+            PlayerView(context).apply {
+                player = mExoPlayer
+            }
+        })
+    }
+}
+
+// For displaying preview in
+// the Android Studio IDE emulator
+@Preview(showBackground = true)
+@Composable
+fun VideoPlayerPreview() {
+    VideoPlayer(urlVideo = "https://cdn.videvo.net/videvo_files/video/free/2020-05/large_watermarked/3d_ocean_1590675653_preview.mp4")
+}
