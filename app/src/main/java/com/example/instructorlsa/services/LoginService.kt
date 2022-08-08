50ddf574048fced7011dd5c35f63fc242ba0a0f4
BlacuttMatias <matiasblacutt16@gmail.com>
2022-08-08 02:03:26 -0300

    Fix login service

--- app/src/main/java/com/example/instructorlsa/services/LoginService.kt
+++ app/src/main/java/com/example/instructorlsa/services/LoginService.kt
@@ -3,21 +3,20 @@ package com.example.instructorlsa.services
 import com.example.instructorlsa.models.LoginPostBody
 import com.example.instructorlsa.models.LoginResponse
 import com.example.instructorlsa.models.ResponseLearningSigns
+import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
 import retrofit2.http.Query
 
-class LoginService {
-    interface LoginApiService{
-        @POST("instructorlsa/login")
-        suspend fun login(@Body body: LoginPostBody): LoginResponse
-    }
+interface LoginApiService{
+    @POST("instructorlsa/login")
+    suspend fun login(@Body body: LoginPostBody): Response<LoginResponse>
+}
 
-    class LoginService {
-        suspend fun login(body: LoginPostBody): LoginResponse {
-            return RetrofitBuilder.getRetrofitInstance()
-                .create(LoginApiService::class.java)
-                .login(body = body)
-        }
+class LoginService {
+    suspend fun login(body: LoginPostBody): Response<LoginResponse> {
+        return RetrofitBuilder.getRetrofitInstance()
+            .create(LoginApiService::class.java)
+            .login(body = body)
     }
 }
