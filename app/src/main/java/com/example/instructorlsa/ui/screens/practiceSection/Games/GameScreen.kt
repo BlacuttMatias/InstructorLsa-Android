d6fa86b21992b8e7ef65a778f4c8e98e184c3c8f
BlacuttMatias <matiasblacutt16@gmail.com>
2022-08-14 02:02:28 -0300

    Add dummy game screen without content

---
+++ app/src/main/java/com/example/instructorlsa/ui/screens/practiceSection/Games/GameScreen.kt
@@ -0,0 +1,36 @@
+package com.example.instructorlsa.ui.screens.practiceSection.startPractice
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.instructorlsa.NavigationRoute
+import com.example.instructorlsa.R
+import com.example.instructorlsa.ui.common.components.FooterSloganAndIcon
+import com.example.instructorlsa.ui.common.components.MainButton
+import com.example.instructorlsa.ui.common.components.TitleText
+import com.example.instructorlsa.ui.common.components.topTabBar.TopTabBarLsa
+
+@Composable
+fun GameScreen(navController: NavController) {
+    val titleText = "Una seña"
+    val titleTopTabBarText = "Una categoria"
+
+    Scaffold(
+        topBar = { TopTabBarLsa(titleText = titleTopTabBarText, navController = navController) }
+    ) {
+        Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally){
+            Spacer(modifier = Modifier.height(20.dp))
+            TitleText(text = titleText)
+            Spacer(modifier = Modifier.height(70.dp))
+        }
+    }
+}
