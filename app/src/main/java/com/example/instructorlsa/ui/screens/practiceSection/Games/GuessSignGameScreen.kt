8a68cb728fdd3d0235ab34a2a5db830701e74f03
BlacuttMatias <matiasblacutt16@gmail.com>
2022-08-16 01:28:31 -0300

    Add animated result icon when select a option in guess sign game

--- app/src/main/java/com/example/instructorlsa/ui/screens/practiceSection/Games/GuessSignGameScreen.kt
+++ app/src/main/java/com/example/instructorlsa/ui/screens/practiceSection/Games/GuessSignGameScreen.kt
@@ -1,14 +1,27 @@
 package com.example.instructorlsa.ui.screens.practiceSection.Games
 
 import android.util.Log
+import androidx.compose.animation.*
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.Scaffold
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.instructorlsa.R
 import com.example.instructorlsa.ui.common.components.MainButton
 import com.example.instructorlsa.ui.common.components.SloganFooterText
 import com.example.instructorlsa.ui.common.components.TitleText
@@ -17,10 +30,12 @@ import com.example.instructorlsa.ui.common.components.topTabBar.TopTabBarLsa
 import com.example.instructorlsa.ui.screens.learningSection.signLearning.components.BackNavigateButton
 import com.example.instructorlsa.ui.screens.learningSection.signLearning.components.NextNavigateButton
 import com.example.instructorlsa.ui.screens.learningSection.signLearning.components.VideoPlayer
+import com.example.instructorlsa.ui.screens.practiceSection.Games.components.AnimatedStateResultIcon
 import com.example.instructorlsa.viewmodels.games.GuessSignScreenViewModel
 import com.example.instructorlsa.viewmodels.signs.SignLearningScreenViewModel
 import org.intellij.lang.annotations.JdkConstants
 
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun GuessSignGameScreen(screenViewModel: GuessSignScreenViewModel, navController: NavController) {
     val titleText = screenViewModel.game.name
@@ -30,13 +45,22 @@ fun GuessSignGameScreen(screenViewModel: GuessSignScreenViewModel, navController
         TitleText(text = titleText)
         Spacer(modifier = Modifier.height(30.dp))
         VideoPlayer(urlVideo = screenViewModel.game.sign.urlVideo)
-        Spacer(modifier = Modifier.height(30.dp))
+        Spacer(modifier = Modifier.height(20.dp))
+        AnimatedStateResultIcon(isVisible = screenViewModel.answerWasSelected,
+            isPositiveResult = (screenViewModel.answerSelected?.isCorrect ?: false)
+        )
+        if(screenViewModel.answerWasSelected){
+            Spacer(modifier = Modifier.height(20.dp))
+        }
+        else{
+            Spacer(modifier = Modifier.height(90.dp))
+        }
+
         screenViewModel.game.answerOptions.forEach { answerOption ->
             OptionButton(text = answerOption.text, showState = screenViewModel.showStateAnswer(answerOption),isCorrect = answerOption.isCorrect) {
                 screenViewModel.didTapAnswerOption(answerOption)
             }
             Spacer(modifier = Modifier.height(20.dp))
         }
-        SloganFooterText(30.dp)
     }
 }
