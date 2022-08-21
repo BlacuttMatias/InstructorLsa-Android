55770a4926cc018c56b1e37163e8b77efeb3a5ba
BlacuttMatias <matiasblacutt16@gmail.com>
2022-08-21 02:58:24 -0300

    Add background color to circle progress animated and increse siza of some ui elements

--- app/src/main/java/com/example/instructorlsa/ui/screens/practiceSection/resultGames/components/ResultCircleProgressAnimated.kt
+++ app/src/main/java/com/example/instructorlsa/ui/screens/practiceSection/resultGames/components/ResultCircleProgressAnimated.kt
@@ -9,6 +9,8 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
@@ -17,18 +19,21 @@ import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.instructorlsa.ui.theme.DarkGrey300
+import com.example.instructorlsa.ui.theme.Grey300
 
 @Composable
 fun ResultCircleProgressAnimated(
     percentage: Float,
     fontSize: TextUnit = 28.sp,
-    size: Dp = 80.dp,
+    size: Dp = 100.dp,
     color: Color = MaterialTheme.colors.primary,
-    strokeWidth: Dp = 8.dp,
+    strokeWidth: Dp = 10.dp,
     animDuration: Int = 1000,
     animDelay: Int = 0
 ){
     var progressPercentage by remember { mutableStateOf(0.0f) }
+    val backgroundCircleColor = MaterialTheme.colors.secondaryVariant
     
     val animatedProgressPercentage by animateFloatAsState(
         targetValue = progressPercentage,
@@ -43,6 +48,22 @@ fun ResultCircleProgressAnimated(
         contentAlignment = Alignment.Center
     ){
         Canvas(modifier = Modifier.size(size)){
+            drawArc(
+                color = backgroundCircleColor,
+                -90f + 360*animatedProgressPercentage,
+                360f,
+                useCenter = false,
+                style = Stroke(strokeWidth.toPx(), cap = StrokeCap.Square)
+            )
+            if(animatedProgressPercentage>0.05){
+                drawArc(
+                    color = color,
+                    -90f,
+                    360*animatedProgressPercentage-10,
+                    useCenter = false,
+                    style = Stroke(strokeWidth.toPx(), cap = StrokeCap.Square)
+                )
+            }
             drawArc(
                 color = color,
                 -90f,
