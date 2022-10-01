7ad7bf12786186b28282ea7e9d38f2c0926daf94
BlacuttMatias <matiasblacutt16@gmail.com>
2022-10-01 16:56:23 -0300

    Use uri from fileProvider to save video

--- app/src/main/java/com/example/instructorlsa/ui/screens/practiceSection/Games/SignTheWordGameScreen.kt
+++ app/src/main/java/com/example/instructorlsa/ui/screens/practiceSection/Games/SignTheWordGameScreen.kt
@@ -7,6 +7,7 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Environment
+import android.os.FileUtils
 import android.os.StrictMode
 import android.os.StrictMode.VmPolicy
 import android.provider.MediaStore
@@ -25,7 +26,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.core.app.ActivityCompat
+import androidx.core.content.FileProvider
+import androidx.core.net.toUri
 import androidx.navigation.NavController
+import com.example.instructorlsa.BuildConfig
 import com.example.instructorlsa.ui.common.components.MainButton
 import com.example.instructorlsa.ui.common.components.TitleText
 import com.example.instructorlsa.viewmodels.games.signTheWord.SignTheWordGameViewModel
@@ -37,7 +41,7 @@ fun SignTheWordGameScreen(screenViewModel: SignTheWordGameViewModel, navControll
     val titleText = screenViewModel.game.name
     val context = LocalContext.current
     var launcher = rememberLauncherForActivityResult(CaptureVideoContract()){
-
+        Log.d("ASDASDASDSADASD", it.toString())
     }
 
     var launcherPermissions = rememberLauncherForActivityResult(ActivityResultContracts.RequestMultiplePermissions()){
@@ -57,45 +61,55 @@ fun SignTheWordGameScreen(screenViewModel: SignTheWordGameViewModel, navControll
         MainButton(text = screenViewModel.getMainButtonText()) {
             launcherPermissions.launch(
                 arrayOf(
+//                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
                     Manifest.permission.CAMERA
-//                    Manifest.permission.WRITE_EXTERNAL_STORAGE
                 )
             )
         }
         if(screenViewModel.hasPermission){
-//            val file = File(context.externalCacheDir,"videocaptureexample.mp4")
-//            val uri = FileProvider.getUriForFile(
-//                context,
-//                BuildConfig.APPLICATION_ID + ".provider",
-//                file
-//            )
+            val file = File(context.filesDir,"example1.mp4")
+            val uri = FileProvider.getUriForFile(
+                context,
+                BuildConfig.APPLICATION_ID + ".provider",
+                file
+            )
+            Log.d("22222222222222222222", file.length().toString())
+            if (!file.exists()) {
+
+                file.createNewFile()
+            }
 //            val builder = VmPolicy.Builder()
 //            StrictMode.setVmPolicy(builder.build())
 //            Log.d("ASDASDASDASD", file.toString() + "  " + file.toURI().toString())
-            launcher.launch((context.externalCacheDir?.path ?: "") + "/videocaptureexample.mp4")
+//            val file = File(context.cacheDir,"videoexample.mp4")
+//            if (!file.exists()) {
+//                file.createNewFile()
+//            }
+            Log.d("11111111111111111", uri.toString())
+            launcher.launch(uri)
 //            val intent = Intent(MediaStore.ACTION_VIDEO_CAPTURE).putExtra(MediaStore.EXTRA_OUTPUT, Environment.getExternalStorageDirectory().path +"videocapture_example.mp4")
 //            startActivity(context, intent, null)
         }
     }
 }
 
-class CaptureVideoContract : ActivityResultContract<String, Boolean>() {
+class CaptureVideoContract : ActivityResultContract<Uri,  Uri?>() {
     @CallSuper
-    override fun createIntent(context: Context, input: String): Intent {
+    override fun createIntent(context: Context, input: Uri): Intent {
         return Intent(MediaStore.ACTION_VIDEO_CAPTURE)
             .putExtra(MediaStore.EXTRA_OUTPUT, input)
             .putExtra(MediaStore.EXTRA_DURATION_LIMIT, 6)
-//            .addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
-//            .addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
+            .addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
+            .addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
     }
 
     final override fun getSynchronousResult(
         context: Context,
-        input: String
-    ): SynchronousResult<Boolean>? = null
+        input: Uri
+    ): SynchronousResult< Uri?>? = null
 
     @Suppress("AutoBoxing")
-    final override fun parseResult(resultCode: Int, intent: Intent?): Boolean {
-        return resultCode == Activity.RESULT_OK
+    final override fun parseResult(resultCode: Int, intent: Intent?): Uri? {
+        return intent.takeIf { resultCode == Activity.RESULT_OK }?.data
     }
 }
