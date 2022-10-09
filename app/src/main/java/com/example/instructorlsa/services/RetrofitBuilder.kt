403137b8a3718637aa6f1a1e2dc29fe607dada67
BlacuttMatias <matiasblacutt16@gmail.com>
2022-10-09 17:14:34 -0300

    Set timeout in requests to 30 seconds

--- app/src/main/java/com/example/instructorlsa/services/RetrofitBuilder.kt
+++ app/src/main/java/com/example/instructorlsa/services/RetrofitBuilder.kt
@@ -1,10 +1,18 @@
 package com.example.instructorlsa.services
 
 import com.example.instructorlsa.viewmodels.InstructorLsaConfig
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object RetrofitBuilder {
+
+    val httpClient = OkHttpClient.Builder()
+        .connectTimeout(30, TimeUnit.SECONDS)
+        .readTimeout(30, TimeUnit.SECONDS)
+        .writeTimeout(30, TimeUnit.SECONDS)
+        .build()
 
     private fun getToken(): String{
         return "Token " + InstructorLsaConfig.getUserToken()
@@ -14,6 +22,7 @@ object RetrofitBuilder {
         return Retrofit.Builder()
             .baseUrl(baseUrl)
             .addConverterFactory(GsonConverterFactory.create())
+            .client(httpClient)
             .build()
     }
 
@@ -21,6 +30,7 @@ object RetrofitBuilder {
         return Retrofit.Builder()
             .baseUrl(baseUrl)
             .addConverterFactory(GsonConverterFactory.create())
+            .client(httpClient)
             .build()
     }
 
