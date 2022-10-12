80d58756ad825644d504301c4f7b84f637d53108
BlacuttMatias <matiasblacutt16@gmail.com>
2022-10-12 01:12:13 -0300

    Show enabled and disabled categories on practice section

--- app/src/main/java/com/example/instructorlsa/ui/screens/categories/components/CategoryCard.kt
+++ app/src/main/java/com/example/instructorlsa/ui/screens/categories/components/CategoryCard.kt
@@ -5,11 +5,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.Card
+import androidx.compose.material.ContentAlpha
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -21,9 +23,15 @@ import com.example.instructorlsa.viewmodels.categories.CategoryViewModel
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun CategoryCard(category: CategoryViewModel, onClick: () -> Unit){
+    var alpha = ContentAlpha.disabled
+    if (category.enabled) {
+        alpha = ContentAlpha.high
+    }
     Card(
         elevation = 10.dp,
-        onClick = onClick
+        onClick = onClick,
+        enabled = category.enabled,
+        modifier = Modifier.alpha(alpha)
     ) {
         Column(modifier = Modifier.fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally
